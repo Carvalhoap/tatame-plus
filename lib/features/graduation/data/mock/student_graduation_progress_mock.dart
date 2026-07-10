dcf05278c8e1808bb5fb65a8d3364100dfa4c9bd
Alexandre Carvalho <apcarvalho85@gmail.com>
2026-07-10 11:26:52 -0300

    feat(graduation): add adult graduation mock data

---
+++ lib/features/graduation/data/mock/student_graduation_progress_mock.dart
@@ -0,0 +1,12 @@
+import '../../models/student_graduation_progress.dart';
+
+final studentGraduationProgressMock = StudentGraduationProgress(
+  id: 'progress_1',
+  academyId: 'academy_1',
+  studentId: '1',
+  graduationProgramId: 'adult_program_1',
+  currentStageId: 'white_belt_degree_2',
+  stageStartedAt: DateTime(2026, 4, 1),
+  validAttendances: 12,
+  estimatedCompletionDate: DateTime(2026, 8, 1),
+);
